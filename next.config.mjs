77336a50bde077470aa01562da56392a1ceda17d
DarkSack <johanjafet4@gmail.com>
2025-07-25 08:06:58 -0600

    fix middleware

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://dn-d-inky.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS"
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization, X-Requested-With, Accept, Origin"
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
