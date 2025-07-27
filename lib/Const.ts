c025cdc9045725aec32b16ef8af99a7b249597d2
DarkSack <johanjafet4@gmail.com>
2025-07-26 19:42:38 -0600

    Last try for today

--- lib/Const.ts
+++ lib/Const.ts
@@ -1,6 +1,4 @@
-const ALLOWED_ORIGIN =
+export const allowedOrigins =
   process.env.NODE_ENV === "production"
-    ? "https://dn-d-inky.vercel.app, https://dn-d-johanjafet4-gmailcoms-projects.vercel.app"
-    : "http://localhost:5173";
-
-export default ALLOWED_ORIGIN;
+    ? ["https://dn-d-inky.vercel.app", "https://dn-d-johanjafet4-gmailcoms-projects.vercel.app"]
+    : ["http://localhost:5173", "http://localhost:3001"];
